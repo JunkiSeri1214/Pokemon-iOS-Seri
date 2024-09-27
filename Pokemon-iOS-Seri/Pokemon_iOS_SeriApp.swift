ce0182c5f2b11964f3aa96ace792934d61d3072c
JunkiSeri <seri@jambo-inc.io>
2024-09-27 15:09:30 +0900

    Initial Commit

---
+++ Pokemon-iOS-Seri/Pokemon_iOS_SeriApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_iOS_SeriApp.swift
+//  Pokemon-iOS-Seri
+//
+//  Created by  瀬利純樹 on 2024/09/27.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_iOS_SeriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
