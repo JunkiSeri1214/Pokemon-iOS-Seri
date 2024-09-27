63ac0703aed7e0bf752509302758c1ff9db6c84b
JunkiSeri <seri@jambo-inc.io>
2024-09-27 17:21:44 +0900

    1個のボールを表示

--- Pokemon-iOS-Seri/ContentView.swift
+++ Pokemon-iOS-Seri/ContentView.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Circle()
+                .trim(from: 0.5, to: 1.0)
+                .fill(.red)
+                .overlay(
+                    Circle()
+                        .trim(from: 0.5, to: 1.0)
+                        .stroke(.black, lineWidth: 1)
+                )
+            Circle()
+                .trim(from: 0, to: 0.5)
+                .fill(.white)
+                .overlay(
+                    Circle()
+                        .trim(from: 0, to: 0.5)
+                        .stroke(.black, lineWidth: 1)
+                )
         }
         .padding()
     }
