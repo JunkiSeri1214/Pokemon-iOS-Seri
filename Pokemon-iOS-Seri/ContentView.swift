93a14093c0ae9c0e8b4688b3218b9148c34f45f7
JunkiSeri1214 <seri@jambo-inc.io>
2024-09-29 18:18:03 +0900

    2列でモンスターボールを表示する

--- Pokemon-iOS-Seri/ContentView.swift
+++ Pokemon-iOS-Seri/ContentView.swift
@@ -8,27 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // レイアウトを2列に設定
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
         
+        // 縦方向にスクロールを設定
         ScrollView {
-            ForEach(1..<152) { index in
-                ZStack {
-                    Circle()
-                        .trim(from: 0.5, to: 1.0)
-                        .fill(.red)
-                        .overlay(
-                            Circle()
-                                .trim(from: 0.5, to: 1.0)
-                                .stroke(.black, lineWidth: 1)
-                        )
-                    Circle()
-                        .trim(from: 0, to: 0.5)
-                        .fill(.white)
-                        .overlay(
-                            Circle()
-                                .trim(from: 0, to: 0.5)
-                                .stroke(.black, lineWidth: 1)
-                        )
+            // 2列で表示
+            LazyVGrid(columns: columns) {
+                // 151個表示
+                ForEach(1..<152) { index in
+                    ZStack {
+                        Circle()
+                            .trim(from: 0.5, to: 1.0)
+                            .fill(.red)
+                            .overlay(
+                                Circle()
+                                    .trim(from: 0.5, to: 1.0)
+                                    .stroke(.black, lineWidth: 1)
+                            )
+                        Circle()
+                            .trim(from: 0, to: 0.5)
+                            .fill(.white)
+                            .overlay(
+                                Circle()
+                                    .trim(from: 0, to: 0.5)
+                                    .stroke(.black, lineWidth: 1)
+                            )
+                    }
                 }
                 .padding()
             }
